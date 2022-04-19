f36a147fbb3a2abbbfe569401a1470d384efce74
MD Shamim Hossain <shamim.cse.gub@gmail.com>
2022-04-19 17:39:08 +0600

    buffer and stream node js

---
+++ readstream.js
@@ -0,0 +1,31 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.write("<html> <head> <title> Form </title> </head>");
+    res.write(
+      '<body> <form method="post" action ="/process"> <input name="message" /> </form></body>'
+    );
+    res.end();
+  } else if (req.url === "/process" && req.method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+
+    req.on("end", () => {
+      console.log("stream finished.");
+      const parsedBody = Buffer.concat(body).toString();
+      console.log(parsedBody);
+
+      res.write("Thank You for submitting.");
+      res.end();
+    });
+  } else {
+    res.write("Not Found.");
+    res.end;
+  }
+});
+server.listen(3000);
+console.log("listening on port 3000");
+
